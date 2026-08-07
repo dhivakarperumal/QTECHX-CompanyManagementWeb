0587a751545b6a408e0a1beb53c3a677480e2a12
Dhivakar <142518349+dhivakarperumal@users.noreply.github.com>
2026-08-07 17:47:51 +0530

    fix it

--- Backend/src/routers/traineeTaskRouter.js
+++ Backend/src/routers/traineeTaskRouter.js
@@ -8,15 +8,15 @@ const taskUpload = upload.single("task_document");
 const assignmentUpload = upload.single("assignment_document");
 
 // Trainee Tasks
-router.get("/trainee-tasks", verifyToken.authenticate, authenticate, traineeTaskController.getTraineeTasks);
-router.post("/trainee-tasks", verifyToken.authenticate, authenticate, taskUpload, traineeTaskController.createTraineeTask);
-router.put("/trainee-tasks/:uuid", verifyToken.authenticate, authenticate, taskUpload, traineeTaskController.updateTraineeTask);
-router.delete("/trainee-tasks/:uuid", verifyToken.authenticate, authenticate, traineeTaskController.deleteTraineeTask);
+router.get("/trainee-tasks", authenticate, traineeTaskController.getTraineeTasks);
+router.post("/trainee-tasks", authenticate, taskUpload, traineeTaskController.createTraineeTask);
+router.put("/trainee-tasks/:uuid", authenticate, taskUpload, traineeTaskController.updateTraineeTask);
+router.delete("/trainee-tasks/:uuid", authenticate, traineeTaskController.deleteTraineeTask);
 
 // Trainee Task Assignments
-router.get("/trainee-task-assignments", authenticate, verifyToken.authenticate, traineeTaskController.getAssignments);
-router.post("/trainee-task-assignments", authenticate, verifyToken.authenticate, assignmentUpload, traineeTaskController.assignTask);
-router.put("/trainee-task-assignments/:uuid", authenticate, verifyToken.authenticate, assignmentUpload, traineeTaskController.updateAssignment);
-router.delete("/trainee-task-assignments/:uuid", authenticate, verifyToken.authenticate, traineeTaskController.deleteAssignment);
+router.get("/trainee-task-assignments", authenticate, traineeTaskController.getAssignments);
+router.post("/trainee-task-assignments", authenticate, assignmentUpload, traineeTaskController.assignTask);
+router.put("/trainee-task-assignments/:uuid", authenticate, assignmentUpload, traineeTaskController.updateAssignment);
+router.delete("/trainee-task-assignments/:uuid", authenticate, traineeTaskController.deleteAssignment);
 
 module.exports = router;
--- Frontend/src/Admin/Trainees/index.js
+++
@@ -1,9 +0,0 @@
-export { default as AllTraineeInterns } from './AllTraineeInterns.jsx';
-export { default as AddTraineeIntern } from './AddTraineeIntern.jsx';
-export { default as TraineeInternDetails } from './TraineeInternDetails.jsx';
-export { default as TraineeInternAttendancePage } from './TraineeInternAttendancePage.jsx';
-export { default as TraineeInternAttendanceView } from './TraineeInternAttendanceView.jsx';
-export { default as TraineeTaskMaster } from './TraineeTaskMaster.jsx';
-export { default as TraineeTaskAssign } from './TraineeTaskAssign.jsx';
-export { default as TraineeTaskDetails } from './TraineeTaskDetails.jsx';
-export { default as TraineeAssignmentDrawer } from './TraineeAssignmentDrawer.jsx';
