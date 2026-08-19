476bef2af80aa01ac038b9082439c0611960abe5
qtechx001 <qtechx001@gmail.com>
2026-08-19 10:49:21 +0530

    event banner size

--- Frontend/src/Componets/Home/EventBanner.jsx
+++ Frontend/src/Componets/Home/EventBanner.jsx
@@ -93,12 +93,12 @@ const EventBanner = () => {
               text-3xl
               font-bold
               uppercase
-              leading-[1.05]
+              leading-[1.2]
               tracking-tight
               text-white
               sm:text-4xl
               md:text-4xl
-              lg:text-[3.2rem]
+              lg:text-[3rem]
             "
             >
               STUDENT
@@ -116,7 +116,7 @@ const EventBanner = () => {
               data-aos-delay="450"
               className="
               mx-auto
-              mt-4
+              mt-2
               max-w-[480px]
               text-sm
               leading-6
